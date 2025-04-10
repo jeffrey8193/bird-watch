79ae4d3ba7fca08d5e1efbd463d88bf9aecd41cc
Jeffrey Jian <jeffreyj8193@gmail.com>
2025-04-09 20:29:49 -1000

    Created initial website

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,14 +5,7 @@ const Home = () => (
   <main>
     <Container id="landing-page" fluid className="py-3">
       <Row className="align-middle text-center">
-        <Col xs={4}>
-          <Image src="next.svg" width="150px" alt="" />
-        </Col>
-
-        <Col xs={8} className="d-flex flex-column justify-content-center">
-          <h1>Welcome to this template</h1>
-          <p>Now get to work and modify this app!</p>
-        </Col>
+        <h1> Middle </h1>
       </Row>
     </Container>
   </main>
